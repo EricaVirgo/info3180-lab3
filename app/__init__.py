10cde8aa62c798cb22f33bfdc4d3ee55a036f1a8
Erica Virgo <virgoerica3@gmail.com>
2019-02-18 04:54:25 +0000

    slight change in init

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SomeRandomPassphrase'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '465' 
-app.config['MAIL_USERNAME'] = 'EricaVirgo'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USERNAME'] = '7e18d9493f0bd9'
+app.config['MAIL_PASSWORD'] = '4c22fdf3acfda2'
 
 mail = Mail(app)
 from app import views
